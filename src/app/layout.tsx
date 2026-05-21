290ac4cb74a1e80ac9dbdb4bd342d5754db5ad67
Getu Hirpo Begna <getu4ever@gmail.com>
2026-05-21 17:36:20 +0100

    clean up layout and remove intrusive tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,23 +39,6 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
-      <head>
-        {/* Travelpayouts Tracking Script */}
-        <Script
-          id="travelpayouts-tracker"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                  var script = document.createElement("script");
-                  script.async = 1;
-                  script.src = 'https://emrldco.com/NTMxMTA5.js?t=531109';
-                  document.head.appendChild(script);
-              })();
-            `,
-          }}
-        />
-      </head>
       <body className="min-h-full flex flex-col bg-slate-50 text-slate-900 selection:bg-indigo-200">
         {children}
       </body>
